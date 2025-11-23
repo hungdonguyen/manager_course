8ce4ff91f9ed48bf426b5152491c4125f3c0134b
hungdonguyen <167590894+hungdonguyen@users.noreply.github.com>
2025-11-23 13:54:55 +0700

    15

--- templates/layouts/footer.php
+++ templates/layouts/footer.php
@@ -1,5 +1,19 @@
 <?php
-if ( ! defined('_HUNG') ) {
+if (! defined('_HUNG')) {
 	die('Truy cập không hợp lệ');
 }
-
+?>
+<!--begin::Footer-->
+<footer class="app-footer">
+	<!--begin::To the end-->
+	<div class="float-end d-none d-sm-inline">Anything you want</div>
+	<!--end::To the end-->
+	<!--begin::Copyright-->
+	<strong>
+		Copyright © 2014-2024&nbsp;
+		<a href="https://adminlte.io" class="text-decoration-none">AdminLTE.io</a>.
+	</strong>
+	All rights reserved.
+	<!--end::Copyright-->
+</footer>
+<!--end::Footer-->
